ab32b50c763cf0b404c8a9230ac97972478723d5
walkerflocker <1042.aw@gmail.com>
2018-07-10 18:20:12 +1200

    removed logs

--- src/m_list/generator.clj
+++ src/m_list/generator.clj
@@ -2,11 +2,7 @@
     (:require [rum.core :as rum]))
 
 (rum/defc wrapper []
-    (println)
-    (println)
     (let [names (.list (clojure.java.io/file "./public"))]
-        (println (count names))
-        ; (println files))
         (for [eachFile names]
             [:div {:style {:margin "10px 0 30px 0"}} eachFile
             [:audio {:style {:display "block"} :src eachFile :controls true}]])))
