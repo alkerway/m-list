47858ed2dfa341cc0660c146d274bb622784c82c
walkerflocker <1042.aw@gmail.com>
2018-07-10 18:12:20 +1200

    generated dynamic audio elements

--- src/m_list/generator.clj
+++ src/m_list/generator.clj
@@ -5,9 +5,11 @@
     (println)
     (println)
     (let [names (.list (clojure.java.io/file "./public"))]
-    (println (count names)))
-    ; (println files))
-    [:h1 "suuu"])
+        (println (count names))
+        ; (println files))
+        (for [eachFile names]
+            [:div {:style {:margin "10px 0 30px 0"}} eachFile
+            [:audio {:style {:display "block"} :src eachFile :controls true}]])))
 
 (defn getAudio []
     (rum/render-html (wrapper)))
