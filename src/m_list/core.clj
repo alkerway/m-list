1507a9f1bcd5f5979f44dafbeb95473ed69c9690
walkerflocker <1042.aw@gmail.com>
2018-07-10 17:30:47 +1200

    base prj

---
+++ src/m_list/core.clj
@@ -0,0 +1,6 @@
+(ns m-list.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/m_list/core_test.clj
@@ -0,0 +1,7 @@
+(ns m-list.core-test
+  (:require [clojure.test :refer :all]
+            [m-list.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
