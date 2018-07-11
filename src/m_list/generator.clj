d90607b267e2d631aba6984d61ceb48dda5e1525
walkerflocker <1042.aw@gmail.com>
2018-07-11 06:39:07 +0000

    file type for audio and video element

--- src/m_list/generator.clj
+++ src/m_list/generator.clj
@@ -1,11 +1,18 @@
 (ns m-list.generator
-    (:require [rum.core :as rum]))
+    (:require 
+	[rum.core :as rum]
+	[clojure.string :as cljstr]))
+
+(def audioFormats ["wav" "flac" "mp3" "aac"])
+(def videoFormats ["mkv" "m3u8" "mov" "mp4" "flv" "avi"])
 
 (rum/defc wrapper []
     (let [names (.list (clojure.java.io/file "./public/m"))]
         (for [eachFile names]
             [:div {:style {:padding "10px 0 10px 0" :border-bottom "1px solid black"}} eachFile
-            [:audio {:style {:display "block"} :src eachFile :controls true}]])))
+            (if (some #(= % (last (cljstr/split eachFile #"\."))) audioFormats)
+		[:audio {:style {:display "block"} :src eachFile :controls true}]
+		[:video {:style {:display "block"} :src eachFile :controls true}])])))
 
 (defn getAudio []
     (rum/render-html (wrapper)))
