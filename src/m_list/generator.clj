87c57589253b94e2ceff7ca30921a756bff1652c
walkerflocker <1042.aw@gmail.com>
2018-07-11 07:16:24 +0000

    check for video formats

--- src/m_list/generator.clj
+++ src/m_list/generator.clj
@@ -9,10 +9,12 @@
 (rum/defc wrapper []
     (let [names (.list (clojure.java.io/file "./public/m"))]
         (for [eachFile names]
-            [:div {:style {:padding "10px 0 10px 0" :border-bottom "1px solid black"}} eachFile
-            (if (some #(= % (last (cljstr/split eachFile #"\."))) audioFormats)
-		[:audio {:style {:display "block"} :src eachFile :controls true}]
-		[:video {:style {:display "block"} :src eachFile :controls true}])])))
+	    (let [extension (last (cljstr/split eachFile #"\."))]
+	            [:div {:style {:padding "10px 0 10px 0" :border-bottom "1px solid black"}} eachFile
+	            (if (some #(= % extension) audioFormats)
+			[:audio {:style {:display "block"} :src eachFile :controls true}]
+		    (if (some #(= % extension) videoFormats)
+			[:video {:style {:display "block"} :src eachFile :controls true}]))]))))
 
 (defn getAudio []
     (rum/render-html (wrapper)))
