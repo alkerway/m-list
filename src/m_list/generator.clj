edc453abd9ccc1e48fcb9a6d729491fed9f2abcd
walkerflocker <1042.aw@gmail.com>
2018-07-15 11:26:15 +1200

    unified html

--- src/m_list/generator.clj
+++ src/m_list/generator.clj
@@ -3,7 +3,7 @@
 	[rum.core :as rum]
 	[clojure.string :as cljstr]))
 
-(def audioFormats ["wav" "flac" "mp3" "aac"])
+(def audioFormats ["wav" "flac" "mp3" "aac" "ogg"])
 (def videoFormats ["mkv" "mov" "mp4" "flv" "avi"])
 
 (rum/defc file-upload-ui []
@@ -14,13 +14,13 @@
 (rum/defc file-list []
 	(let [names (.list (clojure.java.io/file "./public/m"))]
 		(for [eachFile names]
-		(let [extension (last (cljstr/split eachFile #"\."))]
+			(let [extension (last (cljstr/split eachFile #"\."))
+				  isVideo (some #(= % extension) videoFormats)
+				  isAudio (some #(= % extension) audioFormats)]
 				[:div {:style {:padding "10px 0 10px 0" :border-bottom "1px solid black"}} eachFile
-				(if (some #(= % extension) audioFormats)
-					[:audio {:style {:display "block"} :src eachFile :controls true}]
-				(if (some #(= % extension) videoFormats)
-					[:video {:style {:display "block" :height "240px"} :src eachFile :controls true}]
-					[:a {:style {:display "block"} :href eachFile :target "_blank"} "link"]))]))))
+					[(if isVideo :video (if isAudio :audio :a))
+					{:style {:display "block" :height (if isVideo "240px" nil)}
+						:src eachFile :controls true :href eachFile :target "_blank"} "link"]]))))
 
 (rum/defc wrapper []
 	[:div (file-list) (file-upload-ui)])
